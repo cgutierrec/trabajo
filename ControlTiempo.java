b3b552fb0c85d013e79f6e98b34e69aca03d856c
hyanesd <159968999+hyanesd@users.noreply.github.com>
2024-03-23 07:13:23 -0700

    Update ControlTiempo.java
    Código con aplicación del 10% de los requisitos funcionales

--- ControlTiempo.java
+++ ControlTiempo.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in); 
@@ -6,25 +6,25 @@ public class Main {
     System.out.println("¿Usas Instagram?");
     String resp1 = sc.next(); 
 
-    if (resp1.equals ("si")){
+    if (resp1.equalsIgnoreCase ("si")){
       System.out.println("Ingrese el tiempo que la usa diariamente (horas): ");
       Float tiempo_instagram = sc.nextFloat();
     }
     System.out.println("¿Usas TikTok?");
     String resp2 = sc.next();
 
-    if (resp2.equals ("si")){
+    if (resp2.equalsIgnoreCase ("si")){
       System.out.println("Ingrese el tiempo que la usa diariamente (horas): ");
       Float tiempo_tiktok = sc.nextFloat();
     }
     System.out.println("¿Realizas actividad física?");
     String resp3 = sc.next(); 
 
-    if (resp3.equals("si")){
+    if (resp3.equalsIgnoreCase ("si")){
       System.out.println("¿Cuánto tiempo diario dedicas? (horas)");
       Float tiempo_actividad_fisica = sc.nextFloat(); 
     }
-      if ((resp1.equals("si")) || (resp2.equals("si")) || (resp3.equals("si"))){
+      if ((resp1.equalsIgnoreCase ("si")) || (resp2.equalsIgnoreCase ("si")) || (resp3.equalsIgnoreCase ("si"))){
       System.out.println("Datos ingresados correctamente");
     }else{
       System.out.println("No se ingresaron horas para el control");
